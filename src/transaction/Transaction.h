33ea8bee20fe14c15807d33fc0477b6f16c40d3b
wojciech minko <minko@quicktrurnstudio.com>
2026-08-19 22:57:45 +0200

    Add missing files

---
+++ src/transaction/Transaction.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+
+enum class Status {
+    Dispensing,
+    Completed,
+    Failed,
+};
+
+struct Transaction {
+    std::int64_t timestamp;
+    std::string id;
+    Status status;
+};
