b55760072351e57d02c11001f7f77d5adfaabf63
wojciech minko <minko@quicktrurnstudio.com>
2026-08-19 16:05:54 +0200

    Add missing MainWindow files

---
+++ src/MainWindow.cpp
@@ -0,0 +1,17 @@
+#include "MainWindow.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QVBoxLayout>
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("GUI Application");
+
+    auto mainLayout = new QVBoxLayout;
+
+    auto centralWidget = new QWidget(this);
+    centralWidget->setLayout(mainLayout);
+    setCentralWidget(centralWidget);
+}
